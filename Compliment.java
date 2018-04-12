cb2e22bc0f7c0221d8749fade3e55feed9b51177
ese44 <hard@core.com>
2018-04-12 17:54:32 -0500

    Initial commit

---
+++ Compliment.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+/**
+ * Created by by9506zi on 4/12/2018.
+ */
+class Compliment {
+
+    private String text;
+    Compliment(String text){this.text = text;}
+
+    Compliment(){}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ ComplimentController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+/**
+ * Created by by9506zi on 4/12/2018.
+ */
+
+@RestController
+public class ComplimentController {
+
+    private Compliment[] compliments = {
+            new Compliment("You look nice today"),
+            new Compliment("Your code is awesome!"),
+            new Compliment("I hope you have a great day!")
+    };
+
+    private Random rnd = new Random();
+
+    @RequestMapping("/random")
+    ResponseEntity<Compliment> randomCompliment(){
+        Compliment randomCompliment = compliments[rnd.nextInt(compliments.length)];
+        return new ResponseEntity<Compliment>(randomCompliment, HttpStatus.OK);
+    }
+}
